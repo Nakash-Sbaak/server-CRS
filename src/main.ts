b3aa22779136cea0e13b10b6dc1445414a8f4620
the-sabra <omarsabra509@gmail.com>
2024-01-30 12:19:21 +0200

    Update CORS configuration in main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,20 @@ import { ValidationPipe } from '@nestjs/common';
 import { I18nValidationExceptionFilter, I18nValidationPipe } from 'nestjs-i18n';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: '*', // <- allow request from all domains
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      preflightContinue: false,
+      credentials: true,
+    },
+  }); 
   app.useGlobalPipes(new I18nValidationPipe());
   // to validate inputs
   app.useGlobalFilters(
     new I18nValidationExceptionFilter({ detailedErrors: false }),
   );
 
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type, Accept, Authorization',
-  });
   app.setGlobalPrefix('/api/v1');
   await app.listen(process.env.PORT || 3000);
 }
